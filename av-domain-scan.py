0e27f0089b078471f8ead86e6251c91322d8f897
huhuchen <chengengtiger@gmail.com>
2012-06-25 09:23:59 -0500

    amend generator

--- av-domain-scan.py
+++ av-domain-scan.py
@@ -3,23 +3,34 @@
 from invRegex import invert
 import sys, envoy, itertools
 
+##def generate_domain(input_str):
+##    if '?' in input_str:
+##        count = input_str.count('?')
+##        all_str = negex_str()
+##        all_combinations = itertools.product(all_str, repeat=count) 
+##        initial_domain = (list(input_str) for i in range(len(all_str)**count))
+##        for item in initial_domain:
+##            yield ''.join((replace_wildcard(item, all_combinations.next())))
+##    else:
+##        yield input_str
+##
+##def replace_wildcard(str_list, value):
+##    value = (item for item in value)
+##    for key, _value in enumerate(str_list):
+##        if '?' == _value:
+##            str_list[key] = value.next()
+##    return str_list
+
 def generate_domain(input_str):
-    if '?' in input_str:
-        count = input_str.count('?')
+    count = input_str.count('?')
+    if count:
+        prefix, suffix = input_str.split('?'*count)
         all_str = negex_str()
         all_combinations = itertools.product(all_str, repeat=count) 
-        initial_domain = (list(input_str) for i in range(len(all_str)**count))
-        for item in initial_domain:
-            yield ''.join((replace_wildcard(item, all_combinations.next())))
+        for combination in all_combinations:
+            yield prefix + ''.join(combination) + suffix
     else:
         yield input_str
-
-def replace_wildcard(str_list, value):
-    value = (item for item in value)
-    for key, _value in enumerate(str_list):
-        if '?' == _value:
-            str_list[key] = value.next()
-    return str_list
 
 def check_domain_available(domain):
     r = envoy.run('whois %s'%domain)
